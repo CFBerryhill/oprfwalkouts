9c4782abe195a4c5308369f0bdcecb7a74f33f54
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2018-02-22 08:55:55 -0600

    Added Unique constraint for email field

--- reset.sql
+++ reset.sql
@@ -3,7 +3,7 @@ CREATE DATABASE walkout;
 USE walkout;
 CREATE TABLE `attendees` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
