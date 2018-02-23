57731dad578dd40ef0a63a88767d09c815b167bd
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2018-02-23 15:23:41 -0600

    Replaced database reset.sql with npm run reset

--- reset.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS walkout;
-CREATE DATABASE walkout;
-USE walkout;
-CREATE TABLE `attendees` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-);
--- sql.js
+++ sql.js
@@ -2,12 +2,29 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : process.env.DB_HOST || 'localhost',
   user     : process.env.DB_USER || 'root',
-  password : process.env.DB_PASS || '',
-  database : 'walkout'
+  password : process.env.DB_PASS || ''
 });
 connection.connect(function(err) {
   if(err) throw err;
-  console.log("Database connected");
+  console.log("MySQL connected");
 });
+
+function dbUse() {
+  connection.query(`USE ${process.env.DB_NAME || 'walkout'}`, function(err, data) {
+    switch((err) ? err.errno : -1) {
+      case 1049:
+      require('./reset');
+      dbUse();
+      break;
+      case -1:
+      console.log("Database connected");
+      break;
+      default:
+      throw err;
+    }
+  });
+}
+
+dbUse();
 
 module.exports = connection;
