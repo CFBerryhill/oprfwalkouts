27565ec737719cc7512d963c32bc2e7348774745
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2018-02-23 15:33:51 -0600

    Fixed reset.js exit

--- reset.js
+++ reset.js
@@ -12,4 +12,5 @@ sql.query(`DROP TABLE IF EXISTS \`attendees\``, function(err, data) {
 })
 sql.query(`CREATE TABLE \`attendees\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL UNIQUE, \`added\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (\`id\`));`, function(err, data) {
   if  (err) throw err;
+  process.exit();
 });
